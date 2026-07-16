32995309cb86f05258c156c207ebfff6c0d368a2
sfgi-ap <inve2050@gmail.com>
2026-07-15 22:22:07 -0400

    ahorassii

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,49 @@
 import dotenv from "dotenv";
-
-dotenv.config();
-
+import path from "path";
 import express from "express";
 import cors from "cors";
 
+// Tus rutas existentes
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
-
 import pantryRoutes from "./routes/pantry.js";
 import recipeRoutes from "./routes/recipes.js";
 import mealPlanRoutes from "./routes/mealPlans.js";
 import shoopingListRoutes from "./routes/shoopingList.js";
 
-
+dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.json({message:"Ia generator"});
-})
-
-
-app.use("/api/auth",authRoutes);
+// Rutas de API
+app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/pantry", pantryRoutes);
 app.use("/api/recipes", recipeRoutes);
 app.use("/api/meal-plans", mealPlanRoutes);
 app.use("/api/shooping-list", shoopingListRoutes);
 
-const PORT = process.env.PORT;
 
-app.listen(PORT,()=>{
-    console.log("serviir runner")
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "Ia generator" });
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
