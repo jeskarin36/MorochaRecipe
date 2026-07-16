1084c19724900cfc84f2e3a6e77b0814c16ab116
sfgi-ap <inve2050@gmail.com>
2026-07-15 22:24:24 -0400

    espero que si

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,8 @@ app.use("/api/shooping-list", shoopingListRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+ 
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
